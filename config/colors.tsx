7afc6f0d241c498a5edcc3fa128a02183ed6285a
ayusshrathore <ayusshrathore@gmail.com>
2022-11-07 21:18:22 +0530

    Add colors config

---
+++ config/colors.tsx
@@ -0,0 +1,10 @@
+export const colors = {
+	white: "#fff",
+	primary: "#ef835d",
+	secondary: "#2c365a",
+	tertiary: "#85c6d8",
+	gray: "#d1d5db",
+	graylight: "#f3f4f6",
+	graydark: "#4b5563",
+	accent: "#fbcd77",
+};
