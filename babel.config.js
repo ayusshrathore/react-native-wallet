4f120e4fc0958378f3ad9a3bc1da0893ffb08a85
ayusshrathore <ayusshrathore@gmail.com>
2022-11-07 21:22:25 +0530

    Add react-native-reanimated plugins in babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
+		plugins: ["react-native-reanimated/plugin"],
+	};
 };
